6f9a6f6e4084f72940315ff656a0817d163b9df7
Proud Cobol Writer <77924245+proudCobolWriter@users.noreply.github.com>
2023-04-25 12:51:44 +0200

    Fixed tracking

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
-
-/** @type {import('vite').UserConfig} */
-export default defineConfig({
-	plugins: [react()],
-});
